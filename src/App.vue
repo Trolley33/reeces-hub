93a807a46ba2d038340355a8b17f804bb09a2e85
Reece Trolley <reecetrolley@gmail.com>
2020-01-10 22:21:37 +0000

    Begin work on redesign

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      clipped-right
+    >
+      <div class="d-flex align-center main-logo pa-1">
+        <v-img
+          alt="Main Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <span class="headline mr-2 white--text" >Reece's Hub</span>
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            Projects
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in ['Reece\'s Hub', 'Cook-book.co.uk']"
+            :key="index"
+            @click="console.log('clicked' + item)"
+          >
+            <v-list-item-title>{{ item }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-content>
+      <Homepage/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage';
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Homepage,
+  },
+
+  data: () => ({
+    drawer: false
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.main-logo {
+  cursor: pointer;
+  border-radius: 5px;
 }
+
+.main-logo:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+  
+}
+
 </style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ma-3">
+    <vue-timeline-update
+    :date="new Date('2020/01/10')"
+    title="Rebirth!"
+    description="Now starting to work on my portfolio again, isn't that exciting?"
+    thumbnail="https://i.ytimg.com/vi/i1DOTURfpno/maxresdefault.jpg"
+    category="ANNOUNCEMENT"
+    icon="build"
+    color="purple"
+    />
+
+    <vue-timeline-update
+    :date="new Date('2019/10/30')"
+    title="The beginning"
+    description="Bought my own domain and hosting platform, fame here I come!"
+    category="WOW!"
+    icon="code"
+    color="white"
+    is-last
+    />
+  </div>
+</template>
