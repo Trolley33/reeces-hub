7aad04c4e54daf6b3bba6e6c7c1e2f7a70d44cbc
Reece Trolley <reecetrolley@gmail.com>
2020-06-10 03:37:01 +0100

    Restart with Jekyll...

---
+++ _site/assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"My Introduction to C++",'url':"/2020/06/10/intro-to-cpp.html"}]};
--- public/api/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "u259716439_trolley33", "ChelseaTindle69", "u259716439_hub");
-
-?>
