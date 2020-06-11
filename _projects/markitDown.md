c6dee642875902330ee79a987fd2b578dc354beb
Reece Trolley <reecetrolley@gmail.com>
2020-06-12 00:53:04 +0100

    Finalising structure changes

---
+++ _projects/markitDown.md
@@ -0,0 +1,12 @@
+---
+layout: article
+title: MarkItDown
+lightbox: true
+cover: /assets/images/markitdown-cover.png
+---
+
+![MarkItDown v1.0.0](/assets/images/markitdown-cover.png)
+
+- __Language__: JavaScript
+- __Framework__: Vue.js + Electron.js
+- __Github__: [MarkItUp](https://github.com/Trolley33/markbook)
--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Making a Game in SFML: Prelude",'url':"/2020/06/09/sfml-1.html"},{'title':"Making a Game in SFML: Basics",'url':"/2020/06/10/sfml-2.html"}]};
+window.TEXT_SEARCH_DATA={'portfolio':[],'posts':[{'title':"Making a Game in SFML: Prelude",'url':"/2020/06/09/sfml-1.html"},{'title':"Making a Game in SFML: Basics",'url':"/2020/06/10/sfml-2.html"}],'projects':[{'title':"MarkItDown",'url':"/projects/markitDown.html"}]};
